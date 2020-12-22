205ff90388d65f53aee1f49fcfcc5c42545f841e
Martin Lexow <martin.lexow@ixeau.com>
2020-12-22 17:38:57 +0100

    Initial Commit

---
+++ CalendarDemo/CalendarDemoApp.swift
@@ -0,0 +1,12 @@
+// CalendarDemoApp.swift
+
+import SwiftUI
+
+@main
+struct CalendarDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
